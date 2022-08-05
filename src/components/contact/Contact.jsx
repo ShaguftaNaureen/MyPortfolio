bc3431d13b47163fa026301d9a425e23492da564
Mohammed <wmohammed693@gmail.com>
2022-08-06 00:52:07 +0530

    First Commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import './contact.css'
+import {HiOutlineMail} from 'react-icons/hi'
+import {RiMessengerLine} from 'react-icons/ri'
+import {ImWhatsapp} from 'react-icons/im'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_uq95v5k', 'template_rjaqcc6', form.current, '4cdf-SF2qKqpTNPxP')
+
+    e.target.reset()
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <HiOutlineMail className="contact__option-icon"/>
+            <h4>Email</h4>
+            <h5>shaguftanaureen0506@gmail.com</h5>
+            <a href="mailto:shaguftanaureen0506@gmail.com" target="_blank" rel="noreferrer">Send an email</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon"/>
+            <h4>Messenger</h4>
+            <h5>ShaguftaZ</h5>
+            <a href="https://m.me/shagufta" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <ImWhatsapp className="contact__option-icon"/>
+            <h4>WhatsApp</h4>
+            <h5>8660244351</h5>
+            <a href="https://api.whatsapp.com/send?phone=8660244351" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+        </div>
+        {/*END OF CONTACT OPTION */}
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input Type="text" name="name" placeholder='Your Full Name' required/>
+
+          <input type='email' name='email' placeholder="Your Email" required/>
+          <textarea type='message' name="message" rows="7" placeholder="Your Message" required/>
+          <button type='submit' className=" btn btn-primary">Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
 import './testimonials.css'
-import CLIENT1 from '../../assets/client1.png'
+import CLIENT1 from '../../assets/client2.png'
+import { Pagination, Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';   
+
+const data = [
+  {
+    avatar: CLIENT1,
+    name: 'Naina Fatima',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae consequuntur minima, molestiae et libero doloribus reiciendis quod porro suscipit, quibusdam, nobis optio incidunt provident praesentium deleniti culpa amet rem quidem!',
+  },
+  {
+    avatar: CLIENT1,
+    name: 'Khadija',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae consequuntur minima, molestiae et libero doloribus reiciendis quod porro suscipit, quibusdam, nobis optio incidunt provident praesentium deleniti culpa amet rem quidem!',
+  },
+  {
+    avatar: CLIENT1,
+    name: 'Kubra',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae consequuntur minima, molestiae et libero doloribus reiciendis quod porro suscipit, quibusdam, nobis optio incidunt provident praesentium deleniti culpa amet rem quidem!',
+  },
+  {
+    avatar: CLIENT1,
+    name: 'Afreen',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae consequuntur minima, molestiae et libero doloribus reiciendis quod porro suscipit, quibusdam, nobis optio incidunt provident praesentium deleniti culpa amet rem quidem!',
+  },
+]
 
 const Testimonials = () => {
   return (
@@ -8,13 +36,26 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
       
-      <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={CLIENT1} alt="" />
-          </div>
-        </article>
-      </div>
+      <Swiper className="container testimonials__container"
+      modules={[Navigation, Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      >
+        {
+          data.map(({avatar, name, review}, index) => {
+            return(
+              <SwiperSlide key={index} className="testimonial">
+                <div className="client__avatar">
+                  <img src={avatar} alt="" />  
+                </div>
+                <h5 className="client__name">{name}</h5>
+                <small className='client__review'>{review}</small>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </section>
   )
 }
